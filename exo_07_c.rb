7b792883cc58e1eb57b88e30772ab22c440651d2
donatellalnrs <67711636+donatellalnrs@users.noreply.github.com>
2020-07-07 13:56:48 +0200

    Add files via upload

---
+++ exo_07_c.rb
@@ -0,0 +1,3 @@
+user_name = gets.chomp
+puts user_name #les trois programmes différents par la quantité d'info explicite indiquant à l'utilisateur ce qu'il doit renseigner
+#globalement ces trois programmes demandent à l'utilisateur d'entrer une donnée puis l'affichent
